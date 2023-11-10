eb606fbe4de4d6e65532fb65816530dae0649705
mina-45b <bmonroy197@gmail.com>
2023-11-10 18:44:46 +0100

    UF5 - Expresiones Regulares

--- UF5/ExpressionsRegulars/MainClaseString.java
+++ UF5/ExpressionsRegulars/MainClaseString.java
@@ -12,6 +12,49 @@ public class MainClaseString {
         while (scanner.hasNext()) {
             String line = scanner.nextLine();
 
+            String paraNoel = "*<]:-DOo";
+            String rens = ">:o)";
+            String follets = "<]:-D";
+
+            int numParaNoel = 0;
+            int numsRens = 0;
+            int numsFollets = 0;
+
+            int indexParaNoel = line.indexOf(paraNoel);
+            while (indexParaNoel != -1) {
+                numParaNoel++;
+                numsFollets--;
+                indexParaNoel = line.indexOf(paraNoel, indexParaNoel + 1);
+            }
+
+            if (line.contains(rens)) {
+                numsRens++;
+            }
+
+            if (line.contains(follets)) {
+                numsFollets++;
+            }
+
+            String resultat = "";
+
+            if (numParaNoel > 0){
+                resultat += "Pare Noel ("+numParaNoel+") ";
+            }
+
+            if (numsRens > 0) {
+                resultat += "Ren ("+numsRens+") ";
+            }
+
+            if (numsFollets > 0) {
+                resultat += "Follet ("+numsFollets+") ";
+            }
+
+            if (!resultat.isEmpty()){
+                System.out.println(resultat);
+            } else {
+                System.out.println();
+            }
+
         }
 
     }
